9e33515903a199846289e536e0d9ee0b86d786f0
Leonardo Restrepo <wdleonsa@LeonardoRestrepo.local>
2019-03-27 15:02:58 -0500

    Agregando la Aplicación

---
+++ precache-manifest.394015466e44826eb166f839254d377b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ffa100098fbde3b881b4",
+    "url": "/static/js/main.ff5fcb9a.chunk.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "ffa100098fbde3b881b4",
+    "url": "/static/css/main.e077b296.chunk.css"
+  },
+  {
+    "revision": "457d0577a358ad37ae1c9e53e0900795",
+    "url": "/index.html"
+  }
+];
